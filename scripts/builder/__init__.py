6eb074cf0c13e165a484bc22ec3ca7c87159b78f
Daniel J. Finnegan <djf32@bath.ac.uk>
2017-09-22 17:01:17 +0100

    Added the generators to Updater. Now you can generate different project templates

--- scripts/builder/__init__.py
+++ scripts/builder/__init__.py
@@ -1,1 +1,3 @@
 # Empty init.py to house python module
+
+__all__ = ['build_project']
---
+++ scripts/generators/__init__.py
@@ -0,0 +1,9 @@
+# __init__ file for generators module
+# @author Daniel J. Finnegan
+# @date September 2017
+
+__all__ = [
+'base',
+'cpp',
+'unity'
+]
