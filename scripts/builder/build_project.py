949938139e22e3f44c420c648b4504df0de8e303
Daniel J. Finnegan <djf32@bath.ac.uk>
2017-10-23 10:08:10 +0100

    updated the build script to delete the dist and build folders before building

--- scripts/builder/build_project.py
+++ scripts/builder/build_project.py
@@ -4,7 +4,7 @@
 # @author Daniel J. Finnegan
 # @date July 2017
 
-import os, sys
+import os, sys, shutil
 from subprocess import call
 
 UPDATER_CMAKE_ARGS = [] # Required CMake arguments for Updater
@@ -12,6 +12,8 @@ UPDATER_BUILD_CUSTOM = True # Whether or not this project has a custom build mod
 
 def build_full_package(project_root):
 	os.chdir(project_root) # Move back up to the root directory
+	shutil.rmtree(os.path.join(project_root, 'build'), ignore_errors=True)
+	shutil.rmtree(os.path.join(project_root, 'dist'), ignore_errors=True)
 
 	if sys.platform == 'darwin': # Use py2app on OSX
 		call(['python', 'setup.py', 'py2app'])
